a627a8f91c5db2e57a321c7dc2ec48806ecdd094
Brandon Wimberly <Brandon@BrandonMBP-2.local>
2014-01-24 12:41:46 -0600

    Progress on Home Screen Buttons

--- Game Day Local/Gameday_LocalAppDelegate.m
+++ Game Day Local/Gameday_LocalAppDelegate.m
@@ -16,10 +16,10 @@
 {
     
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
-    [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0xbe0f34)];
+    [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0x740309)];
     
     [[UITabBar appearance] setBarTintColor:UIColorFromRGB(0x000000)];
-    [[UITabBar appearance] setSelectionIndicatorImage:[UIImage imageNamed:@"icon-selected"]];
+    [[UITabBar appearance] setSelectionIndicatorImage:[UIImage imageNamed:@"icons-selected"]];
     
     
     // Override point for customization after application launch.
